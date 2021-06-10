e9a73f0380857852ccb59144c09686f863eed0e1
Glennpedley <80082735+Glennpedley@users.noreply.github.com>
2021-06-10 14:46:24 +0800

    Creeating python file
    Took from cloned repository

---
+++ main.py
@@ -0,0 +1,16 @@
+  
+import os
+from twilio.rest import Client
+
+account_sid = os.environ['account_sid']
+auth_token = os.environ['auth_token']
+
+client = Client(account_sid, auth_token)
+
+message = client.messages.create(
+                              body='THIS is MY notification from GitHub-Slayer',
+                              from_='whatsapp:+14155238886',
+                              to='whatsapp:'+os.environ['to_whatsapp_no']
+                          )
+
+print("Message ID:",message.sid)
