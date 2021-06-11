ec2ebcded79c02849358583ed026f89b5b969eba
Glennpedley <80082735+Glennpedley@users.noreply.github.com>
2021-06-11 10:11:00 +0800

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ auth_token = os.environ['auth_token']
 client = Client(account_sid, auth_token)
 
 message = client.messages.create(
-                              body='THIS is MY notification from GitHub-Slayer',
+                              body='THIS is MY notification from GitHub via Twilio-Jimmmy the Virgin Slayer',
                               from_='whatsapp:+14155238886',
                               to='whatsapp:'+os.environ['to_whatsapp_no']
                           )
